62e832b5a6a61f99129314fbe065686eac62c169
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-18 21:22:16 +0530

    InstaDSA

--- BestTimeBuySell.java
+++ BestTimeBuySell.java
@@ -1,5 +1,20 @@
 public class BestTimeBuySell {
-    public static void main(String[] args) {
+    public static int buyAndSell(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
 
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 6, 4 };
+        System.out.println(buyAndSell(prices));
     }
 }
--- LongestSubArraySumK.java
+++ LongestSubArraySumK.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class LongestSubArraySumK {
+    public static int longestSubArr(int nums[], int target) {
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - target)) {
+                // ans += map.get(sum - target);
+                ans = Math.max(ans, i-map.get(sum - target));
+            }
+            // map.put(sum, map.getOrDefault(sum, 0) + 1);
+            map.put(sum, i);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        
+        int nums[] = { 10, 5, 2, 7, 1, 9 };
+        System.out.println(longestSubArr(nums, 15));
     }
 }
